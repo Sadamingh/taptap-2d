ae9738393c4766ffddf673efef4ee083ce54799e
murphylyr <murphylyr@gmail.com>
2024-10-17 00:25:22 -0600

    add npc dialogue system

---
+++ Assets/Scripts/Dialogue System/NPC.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public abstract class NPC : MonoBehaviour, IInteractable
+{
+    [SerializeField] private SpriteRenderer _interactSprite;
+    private Transform _playerTransform;
+    [SerializeField] private const float INTERACT_DISTANCE = 4f; //distance between cat and Interactable NPCs
+
+    private void Start()
+    {
+        _playerTransform = GameObject.FindGameObjectWithTag("Cat").transform;
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && IsWithinInteractDistance())
+        {
+            //talk and interact
+            Interact();
+        }
+        if (_interactSprite.gameObject.activeSelf && !IsWithinInteractDistance())
+        {
+            //turn off sprite
+            _interactSprite.gameObject.SetActive(false);
+        }
+        else if (!_interactSprite.gameObject.activeSelf && IsWithinInteractDistance())
+        {
+            //turn on sprite
+            _interactSprite.gameObject.SetActive(true);
+        }
+    }
+    public abstract void Interact();
+
+    private bool IsWithinInteractDistance()
+    {
+        if (Vector2.Distance(_playerTransform.position, transform.position) < INTERACT_DISTANCE)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ Assets/Scripts/Dialogue System/Test_Cat.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Cat : NPC, ITalkable
+{
+    [SerializeField] private DialogueText dialogueText;
+    [SerializeField] private DialogueController dialogueController;
+    public override void Interact()
+    {
+        // Put actions for NPC (Test_Cat, in this case) below
+        Talk(dialogueText);
+
+    }
+
+    public void Talk(DialogueText dialogueText)
+    {
+        //Start conversation
+        dialogueController.DisplayNextParagraph(dialogueText);
+    }
+}
